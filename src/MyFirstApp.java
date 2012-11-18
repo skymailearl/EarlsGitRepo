3e5b2aaabc38016e6e3e3cb306ef57efba8fcd18
Earl Lynch <earl@skywatching.org>
2012-11-18 12:43:04 -0500

    First exercise

---
+++ src/MyFirstApp.java
@@ -0,0 +1,12 @@
+public class MyFirstApp {
+	   public static void main (String[] args) {
+	     int x = 1;
+	     System.out.println();
+	     while (x < 100) {
+	       System.out.println("In the loop");
+	       System.out.println("Value of x is " + x);
+	         x = x + 1;
+	     }
+	     System.out.println("This is after the loop");
+	   }
+	}
